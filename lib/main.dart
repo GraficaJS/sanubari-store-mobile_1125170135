b0ace9dffa8f195739e625813237f8cbaf979042
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-06-01 15:33:07 +0700

    uas langkah 15 menambahkan di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'screens/auth/login_page.dart';
+import 'screens/dashboard_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
