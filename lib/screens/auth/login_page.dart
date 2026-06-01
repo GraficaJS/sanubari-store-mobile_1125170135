d180e41cbb9acb19f304efdf0fe618573116c21d
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-06-01 14:39:40 +0700

    uas langkah 7

---
+++ lib/screens/auth/login_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              const Icon(
+                Icons.shopping_bag,
+                size: 80,
+                color: Colors.blue,
+              ),
+
+              const SizedBox(height: 20),
+
+              const Text(
+                "Selamat Datang",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              const Text(
+                "Masuk ke akun Sanubari Store",
+              ),
+
+              const SizedBox(height: 30),
+
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Masuk"),
+                ),
+              ),
+
+              const SizedBox(height: 15),
+
+              TextButton(
+                onPressed: () {},
+                child: const Text("Daftar"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
