8cfd3985de718fe6486105bc725e937399d36a89
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-06-01 15:35:23 +0700

    uas langkah 16 menambahkan di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
 import 'screens/auth/login_page.dart';
 import 'screens/dashboard_page.dart';
@@ -24,8 +25,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Sanubari Store',
+
+      routes: {
+        '/dashboard': (context) => const DashboardPage(),
+      },
+
       home: const LoginPage(),
     );
   }
 }
-
