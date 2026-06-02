a3f92656f69e54fac2f8964fdee61ae6609a0c41
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-06-02 14:07:31 +0700

    uas langkah 31

--- sanubari_backend/controllers/product_controller.go
+++ sanubari_backend/controllers/product_controller.go
@@ -2,17 +2,64 @@ package controllers
 
 import (
 	"net/http"
+	"sanubari_backend/config"
+	"sanubari_backend/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetProducts(c *gin.Context) {
 
+	rows, err := config.DB.Query(`
+		SELECT
+		id,
+		name,
+		brand,
+		description,
+		image_url,
+		price,
+		stock
+		FROM products
+	`)
+
+	if err != nil {
+
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"error": err.Error(),
+			},
+		)
+
+		return
+	}
+
+	defer rows.Close()
+
+	var products []models.Product
+
+	for rows.Next() {
+
+		var product models.Product
+
+		rows.Scan(
+			&product.ID,
+			&product.Name,
+			&product.Brand,
+			&product.Description,
+			&product.ImageURL,
+			&product.Price,
+			&product.Stock,
+		)
+
+		products = append(
+			products,
+			product,
+		)
+	}
+
 	c.JSON(
 		http.StatusOK,
-
-		gin.H{
-			"message": "API SANUBARI STORE",
-		},
+		products,
 	)
 }
