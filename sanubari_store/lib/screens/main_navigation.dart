87a1a82aca90335c87f2a4139fdc2f9ca7423d12
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-06-02 15:55:23 +0700

    uas langkah 48 main navigasi

---
+++ sanubari_store/lib/screens/main_navigation.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'profile_page.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() =>
+      _MainNavigationState();
+}
+
+class _MainNavigationState
+    extends State<MainNavigation> {
+
+  int selectedIndex = 0;
+
+  final List<Widget> pages = const [
+
+    DashboardPage(),
+
+    CategoryPage(),
+
+    CartPage(),
+
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: pages[selectedIndex],
+
+      bottomNavigationBar:
+          BottomNavigationBar(
+
+        currentIndex: selectedIndex,
+
+        onTap: (index) {
+
+          setState(() {
+
+            selectedIndex = index;
+          });
+        },
+
+        items: const [
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Category',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
