26a3ee21a36735da4d01044eeb60994458cbeaa6
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-06-02 14:06:58 +0700

    uas langkah 30

--- sanubari_backend/models/product.go
+++ sanubari_backend/models/product.go
@@ -1,17 +1,11 @@
 package models
 
 type Product struct {
-	ID int `json:"id"`
-
-	Name string `json:"name"`
-
-	Brand string `json:"brand"`
-
-	Description string `json:"description"`
-
-	ImageURL string `json:"image_url"`
-
-	Price float64 `json:"price"`
-
-	Stock int `json:"stock"`
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Brand       string  `json:"brand"`
+	Description string  `json:"description"`
+	ImageURL    string  `json:"image_url"`
+	Price       float64 `json:"price"`
+	Stock       int     `json:"stock"`
 }
